3a0e9f058eca1332fd44260699de6c70301bf543
larissaSara <larysara96@gmail.com>
2018-03-07 18:36:37 -0400

    delete pessoa

--- src/main/java/com/example/restful/api/model/Pessoa.java
+++ src/main/java/com/example/restful/api/model/Pessoa.java
@@ -1,6 +1,5 @@
 package com.example.restful.api.model;
 
-
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -28,6 +27,23 @@ public class Pessoa {
 	@Embedded
 	private Endereco endereco;
 	
+	
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
 	public String getNome() {
 		return nome;
 	}
@@ -44,5 +60,30 @@ public class Pessoa {
 		this.ativo = ativo;
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+
 	
 }
---
+++ src/main/resources/db/migration/V02__criar_e_registrar_pessoas.sql
@@ -0,0 +1,2 @@
+CREATE TABLE pessoa (codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT, nome varchar(50) not null, ativo boolean not null, logradouro varchar(100), numero integer, complemento varchar(50), bairro varchar(100), cep varchar(8), cidade varchar(100), estado varchar(100) ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
